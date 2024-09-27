0577ecf3f532bb80119e8dbe47cace19d30a4a20
Navneet Aman <contact@navneetaman.com>
2024-09-27 09:18:19 +0530

    macro based module.

--- modules/fs/src/lib.rs
+++ modules/fs/src/lib.rs
@@ -1,3 +1,9 @@
-mod fs;
-use fs::Fs;
-rquickjs::module_init!(Fs);
+#[rquickjs::module]
+mod fs {
+    #[rquickjs::function]
+    pub fn read_file(path: String) -> String {
+        let rv = std::fs::read_to_string(path).unwrap();
+        return rv;
+    }
+}
+rquickjs::module_init!(js_fs);
