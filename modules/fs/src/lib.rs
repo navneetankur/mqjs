661e9146d9aeb3676dcbbe922248d876886427f7
Navneet Aman <contact@navneetaman.com>
2024-09-28 07:54:41 +0530

    use snake_case instead of camel. Let's remain close to rust.

--- modules/fs/src/lib.rs
+++ modules/fs/src/lib.rs
@@ -6,12 +6,12 @@ mod outer {
     use std::{fs::File, io::{BufRead, BufReader}};
     use common::iterator::JsIterator;
     #[rquickjs::function]
-    pub fn readFile(path: String) -> String {
+    pub fn read_file(path: String) -> String {
         let rv = std::fs::read_to_string(path).unwrap();
         return rv;
     }
     #[rquickjs::function]
-    pub fn readFileLines(filename: String) -> JsIterator<std::io::Lines<BufReader<File>>> {
+    pub fn read_file_lines(filename: String) -> JsIterator<std::io::Lines<BufReader<File>>> {
         let file = File::open(filename).unwrap();
         let br = BufReader::new(file);
         let lines = br.lines();
