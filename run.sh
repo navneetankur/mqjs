9c8e46cc42bec4618c4c00ea921c73b14735f84e
Navneet Aman <contact@navneetaman.com>
2024-09-29 21:59:40 +0530

    remove fs module as file handeling will be in api crate. So integrated in binary.

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 cd /home/navn/workspace/rust/mqjs
 cargo build --release
-cp target/release/mqjs ~/bin
-mkdir -p ~/bin/lib/mqjs/modules/so
-cp target/release/libfs.so ~/bin/lib/mqjs/modules/so
+cp --debug -u target/release/mqjs ~/bin
+# mkdir -p ~/bin/lib/mqjs/modules/so
+# cp target/release/libfs.so ~/bin/lib/mqjs/modules/so
