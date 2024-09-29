b7731eac2799012e2fd72a4fbed3b783c8897ac0
Navneet Aman <contact@navneetaman.com>
2024-09-29 12:14:44 +0530

    command api working in js.

--- api/src/command.rs
+++ api/src/command.rs
@@ -133,70 +133,80 @@ impl JsCommand {
         self.v.env(key, val);
     }
 }
-fn arg(mut this: This<OwnedBorrowMut<JsCommand>>, arg: String) {
+fn arg(mut this: This<OwnedBorrowMut<JsCommand>>, arg: String) -> Class<JsCommand>{
     this.v.arg(arg);
+    return this.0.into_inner();
 }
-fn args(mut this: This<OwnedBorrowMut<JsCommand>>, args: Vec<String>) {
+fn args(mut this: This<OwnedBorrowMut<JsCommand>>, args: Vec<String>) -> Class<JsCommand>{
     this.v.args(args);
+	return this.0.into_inner();
 }
-fn current_dir(mut this: This<OwnedBorrowMut<JsCommand>>, dir: String) {
+fn current_dir(mut this: This<OwnedBorrowMut<JsCommand>>, dir: String) -> Class<JsCommand>{
     this.v.current_dir(dir);
+	return this.0.into_inner();
 }
-fn env(mut this: This<OwnedBorrowMut<JsCommand>>, key: String, val: String) {
+fn env(mut this: This<OwnedBorrowMut<JsCommand>>, key: String, val: String) -> Class<JsCommand>{
     this.v.env(key, val);
+	return this.0.into_inner();
 }
 #[allow(clippy::needless_pass_by_value)]
-fn envs(mut this: This<OwnedBorrowMut<JsCommand>>, jsobj: Object) {
+fn envs<'js>(mut this: This<OwnedBorrowMut<'js, JsCommand>>, jsobj: Object<'js>) -> Class<'js, JsCommand>{
     for entry in jsobj.props::<String, String>() {
         let Ok((k,v)) = entry else {continue};
         this.env(k, v);
     }
+	return this.0.into_inner();
 }
-fn output(mut this: This<OwnedBorrowMut<JsCommand>>) -> output::Output {
-    let out = this.v.output().unwrap();
-    Output { out: out.stdout, err: out.stderr, exitcode: out.status.code() }
-}
-fn status(mut this: This<OwnedBorrowMut<JsCommand>>) -> Option<i32> {
-    this.v.status().unwrap().code()
-}
-fn stdin_null(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdin_null(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdin(Stdio::null());
+	return this.0.into_inner();
 }
-fn stdin_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) {
+fn stdin_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) -> Class<JsCommand>{
     let file = File::open(path).unwrap();
     this.v.stdin(file);
+	return this.0.into_inner();
 }
-fn stdin_piped(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdin_piped(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdin(Stdio::piped());
+	return this.0.into_inner();
 }
-fn stdin_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdin_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdin(Stdio::inherit());
+	return this.0.into_inner();
 }
-fn stdout_null(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdout_null(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdout(Stdio::null());
+	return this.0.into_inner();
 }
-fn stdout_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) {
+fn stdout_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) -> Class<JsCommand>{
     let file = File::open(path).unwrap();
     this.v.stdout(file);
+	return this.0.into_inner();
 }
-fn stdout_piped(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdout_piped(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdout(Stdio::piped());
+	return this.0.into_inner();
 }
-fn stdout_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stdout_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stdout(Stdio::inherit());
+	return this.0.into_inner();
 }
-fn stderr_null(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stderr_null(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stderr(Stdio::null());
+	return this.0.into_inner();
 }
-fn stderr_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) {
+fn stderr_filepath(mut this: This<OwnedBorrowMut<JsCommand>>, path: String) -> Class<JsCommand>{
     let file = File::open(path).unwrap();
     this.v.stderr(file);
+	return this.0.into_inner();
 }
-fn stderr_piped(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stderr_piped(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stderr(Stdio::piped());
+	return this.0.into_inner();
 }
-fn stderr_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) {
+fn stderr_inherit(mut this: This<OwnedBorrowMut<JsCommand>>) -> Class<JsCommand>{
     this.v.stderr(Stdio::inherit());
+	return this.0.into_inner();
 }
 fn spawn(mut this: This<OwnedBorrowMut<JsCommand>>) -> std::io::Result<JsChild> {
     this.v.spawn().map(std::convert::Into::into)
@@ -205,4 +215,11 @@ fn spawn(mut this: This<OwnedBorrowMut<JsCommand>>) -> std::io::Result<JsChild> 
 fn exec(mut this: This<OwnedBorrowMut<JsCommand>>) {
     use std::os::unix::process::CommandExt;
     this.v.exec();
+}
+fn output(mut this: This<OwnedBorrowMut<JsCommand>>) -> output::Output {
+    let out = this.v.output().unwrap();
+    Output { out: out.stdout, err: out.stderr, exitcode: out.status.code() }
+}
+fn status(mut this: This<OwnedBorrowMut<JsCommand>>) -> Option<i32> {
+    this.v.status().unwrap().code()
 }
--- api/src/command/child.rs
+++ api/src/command/child.rs
@@ -9,6 +9,15 @@ pub struct JsChild {
 
 #[rquickjs::methods]
 impl JsChild {
+    pub fn stdout(&mut self) -> std::option::Option<JsChildStdout> {
+        self.v.stdout.take().map(std::convert::Into::into)
+    }
+    pub fn stdin(&mut self) -> std::option::Option<JsChildStdin> {
+        self.v.stdin.take().map(std::convert::Into::into)
+    }
+    pub fn stderr(&mut self) -> std::option::Option<JsChildStderr> {
+        self.v.stderr.take().map(std::convert::Into::into)
+    }
     pub fn kill(&mut self) -> i32 {
         if let Ok(()) = self.v.kill() {
             return 0;
@@ -30,51 +39,42 @@ impl From<Child> for JsChild {
         Self { v: value }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 impl<'js> Trace<'js> for JsChild {
-    fn trace<'a>(&self, tracer: rquickjs::class::Tracer<'a, 'js>) {}
+    fn trace<'a>(&self, _: rquickjs::class::Tracer<'a, 'js>) {}
 }
 #[rquickjs::class]
 pub struct JsChildStdin{
     v: ChildStdin,
 }
+impl From<ChildStdin> for JsChildStdin {
+    fn from(value: ChildStdin) -> Self {
+        Self { v: value }
+    }
+}
 impl<'js> Trace<'js> for JsChildStdin {
-    fn trace<'a>(&self, tracer: rquickjs::class::Tracer<'a, 'js>) {}
+    fn trace<'a>(&self, _: rquickjs::class::Tracer<'a, 'js>) {}
 }
 #[rquickjs::class]
 pub struct JsChildStdout{
     v: ChildStdout,
 }
+impl From<ChildStdout> for JsChildStdout {
+    fn from(value: ChildStdout) -> Self {
+        Self { v: value }
+    }
+}
 impl<'js> Trace<'js> for JsChildStdout {
-    fn trace<'a>(&self, tracer: rquickjs::class::Tracer<'a, 'js>) {}
+    fn trace<'a>(&self, _: rquickjs::class::Tracer<'a, 'js>) {}
 }
 #[rquickjs::class]
 pub struct JsChildStderr{
     v: ChildStderr,
 }
+impl From<ChildStderr> for JsChildStderr {
+    fn from(value: ChildStderr) -> Self {
+        Self { v: value }
+    }
+}
 impl<'js> Trace<'js> for JsChildStderr {
-    fn trace<'a>(&self, tracer: rquickjs::class::Tracer<'a, 'js>) {}
+    fn trace<'a>(&self, _: rquickjs::class::Tracer<'a, 'js>) {}
 }
